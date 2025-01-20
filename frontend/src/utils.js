df6cf2ceee8d99927d91c0c050f58d6c60e8fc01
Fahad <145900785+Fahad00001@users.noreply.github.com>
2025-01-20 20:56:23 +0530

    Update utils.js

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -3,4 +3,4 @@ import {toast} from 'react-toastify'
 export const notify=(message,type)=>{
     toast[type](message)
 }
-export const API_URL="https://employee-management-deploy-api.vercel.app"
+export const API_URL="https://mern-task-manager-app-deploy-api10.vercel.app"
